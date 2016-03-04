4aac2de6826c9274428fafd7370f2ffdc66a662c
Marito <mariobrassescogarcia@gmail.com>
2016-03-04 08:46:19 +0100

    Changed comments into English

---
+++ caesar.rb
@@ -0,0 +1,50 @@
+
+
+# Firstly we declare the variable (the sentence that will be encrypted)
+# We make the user write its value
+
+input = ""
+
+puts "Introduce una frase (sin números)"
+input = gets.chomp
+
+# We make the string to only include alphanumerical characters (no punctuation allowed). Punctuation signals are converted into whitespace.
+
+string = input.gsub(/[^a-z0-9\s]/i, '')
+
+
+
+# We ask the user to introduce the encryption rule (integer number)
+
+puts "Introduce regla de encriptación (número entero)"
+n = gets.chomp.to_i
+
+
+def solve_cipher(string, n)
+  string.split('').map do |x|
+    new_index = x.ord + n
+ 
+
+if x == ' '
+   new_index = ' '.ord - 26
+end
+
+if new_index > 'z'.ord
+      new_index = 'a'.ord + new_index - 'z'.ord - 1
+end
+
+
+if new_index < 'a'.ord
+   new_index = 'z'.ord - ('a'.ord - new_index) + 1
+end
+    
+   new_index.chr
+
+end.join
+
+end
+
+
+puts solve_cipher(string,n)
+
+
